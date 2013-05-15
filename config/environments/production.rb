1b7df12dbe44d2ef86f03d3bb2f15a04ec26b31f
Isarax <isarax@gmail.com>
2013-05-15 23:48:19 +0400

    Precompiled with regular expression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,12 @@ Cinematron::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.assets.precompile = [/^[-_a-zA-Z0-9]*\..*/]
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
