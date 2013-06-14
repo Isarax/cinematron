c5248815897d3568997c061a8cf7d94c3e1dd12b
Isarax <isarax@gmail.com>
2013-06-14 13:05:10 +0400

    delete old files

--- db/migrate/20130606092823_create_roles_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateRolesUsers < ActiveRecord::Migration
-  def change
-    create_table :roles_users, :id => false do |t|
-      t.references :user
-      t.references :role
-    end
-  end
-end
